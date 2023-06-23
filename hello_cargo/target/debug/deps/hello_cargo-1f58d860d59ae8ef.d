b4d0b310d5900f4255f8390f720b55cf19246e7d
Fajar Satria <fajarsat@hotmail.com>
2023-06-24 06:02:30 +0700

    learn basics
    learn basics

---
+++ hello_cargo/target/debug/deps/hello_cargo-1f58d860d59ae8ef.d
@@ -0,0 +1,5 @@
+/Users/satria/Documents/GitHub/learn-rust-basics/hello_cargo/target/debug/deps/hello_cargo-1f58d860d59ae8ef.rmeta: src/main.rs
+
+/Users/satria/Documents/GitHub/learn-rust-basics/hello_cargo/target/debug/deps/hello_cargo-1f58d860d59ae8ef.d: src/main.rs
+
+src/main.rs:
--- test_new/src/main.rs
+++ test_new/src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
     println!("Hello, world!");
+    println!("Input New Char");
+    let mut guess: String = String::new();
+
+    io::stdin().read_line(&mut guess).expect("failed to read the input");
+    print!("you guessed {}",guess );
+
 }
